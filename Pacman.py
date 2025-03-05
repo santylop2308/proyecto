27d8da328ea2cb1a35c0f97a4bbc6f3342f062e2
santylop2308 <technologyrooms@colsanjose.edu.co>
2025-03-05 09:50:46 -0500

    laberinto

--- Pacman.py
+++ Pacman.py
@@ -1,25 +1,90 @@
 import turtle
 import sys, pygame
+import random
 
 pygame.init()
-size = 1200, 800
-screen = pygame.display.set_mode(size)
+width = 1200
+height = 800
+cell_size=40
+fps=10
+screen = pygame.display.set_mode((width,height))
 pygame.display.set_caption("PacMan")
-width, height = 800, 600
-speed = [1, 1]
-pacman = pygame.image.load("pacman.png")
+
+speed = [0, 0]
+BLANCO = (255, 255, 255)
+NEGRO = (0, 0, 0)
+AMARILLO = (255, 255, 0)
+AZUL = (0,0,255)
+pacman = pygame.image.load("pacmanright.png")
+ghosts_imgs=[
+    
+]
 ballrect = pacman.get_rect()
-run = True
+tamaño_celda = 40
+tablero = [ 
+    "##########################" , 
+    "#............##............#" , 
+    "#.####.#####.#####.#####.#" , 
+    "#o###.#####.##.#####.####o#" , 
+    "#.####.#####.#####.####.####.#" , 
+    "#..........................#" , 
+    "#.####.########.#####.####.#" , 
+    "#.####.########.#####.####.#" ,
+    "#......##....##.##......#" , 
+    "######.##### ## #####.######" , 
+    "######.##### ## #####.######" , 
+    "######.## ##.######" , 
+    "######.## ###--### ##.######" , 
+    "######.## # # ##.######" , 
+    " ## # # ##" , 
+    "######.## # # ##.######" , 
+    "######.## ######## ##.######" , 
+    "######.## ##.######" , 
+    "######.## ######## ##.######" , 
+    "######.## ######## ##.######" , 
+    "#............##............#" , 
+    "#.####.#####.##.#####.####.#" , 
+    "#.####.#####.##.#####.####.#" , 
+    "#o..##................##..o#" , 
+    "###.##.##.########.##.##.###" , 
+    "###.##.##.########.##.##.###" , 
+    "#......##....##....##......#" , 
+    "#.##########.##.##########.#" , 
+    "#.##########.##.#########.#" , 
+    "#............................#" , 
+    "############################"
+ ]
+
+def draw_board (): 
+    for y, row in enumerate (tablero):
+        for x, cell in enumerate(row):
+            if cell == "#":
+                pygame.draw.rect(screen, AZUL, (x*cell_size, y * cell_size, cell_size, cell_size))
+            elif cell== ".":
+                pygame.draw.circle (screen, BLANCO, (x*cell_size + cell_size//2, y*cell_size+ cell_size//2),3)
+            elif cell=="o":
+                pygame.draw.circle(screen, BLANCO,(x+cell_size+cell_size//2,y*cell_size+cell_size//2),7)
+run = True  
 
 while run:
     pygame.time.delay(2)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    draw_board()
     keys=pygame.key.get_pressed()
     if keys[pygame.K_UP]:
         ballrect=ballrect.move(0,-1)
+        pacman=pygame.image.load("pacmanup.png")
+    if keys[pygame.K_DOWN]:
+        ballrect=ballrect.move(0,1)
+        pacman=pygame.image.load("pacmandown.png")
+    if keys[pygame.K_RIGHT]:
+        ballrect=ballrect.move(1,0)
+        pacman=pygame.image.load("pacmanright.png")
+    if keys[pygame.K_LEFT]:
+        ballrect=ballrect.move(-1,0)
+        pacman=pygame.image.load("pacmanleft.png")
     ballrect = ballrect.move(speed)
     if ballrect.left < 0 or ballrect.right > width:
         speed[1] = -speed[1]
